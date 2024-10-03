cab9aaa19efd9e393602d1d7a596b1af16a488db
Ethan McDonald <118023721+emcodnald@users.noreply.github.com>
2024-10-03 12:52:35 -0400

    Update platEngine.py

--- platEngine.py
+++ platEngine.py
@@ -1,11 +1,13 @@
 import pygame, sys
 from pygame.locals import *
+sys.path.append("C:\\Users\\hates\\Desktop\\modules")
 import parametric
 import time
 screen = pygame.display.set_mode((640,480))
 screen.fill((255,255,255))
 pygame.init()
 
+cam = [0,0]
 class player:
     def __init__(self):
         self.pos = parametric.orderedPair(320,240)
@@ -13,18 +15,18 @@ class player:
         self.moveState = "still"
         self.fall = True
     def disp(self):
-        pygame.draw.rect(screen, (128,128,128), (self.pos.x-10,self.pos.y-10,20,20))
+        pygame.draw.rect(screen, (128,128,128), (self.pos.x-10+cam[0],self.pos.y-10+cam[1],20,20))
 
 class block:
     def __init__(self,pos):
         self.pos = pos
     def disp(self,p):
-        pygame.draw.rect(screen, (0,0,0), (self.pos.x-10,self.pos.y-10,20,20))
-        if p.pos.x+10+p.vel.x > self.pos.x-10 and p.pos.x-10 < self.pos.x+10 and p.pos.y+10 > self.pos.y-10 and p.pos.y-10 < self.pos.y+10:
+        pygame.draw.rect(screen, (0,0,0), (self.pos.x-10+cam[0],self.pos.y-10+cam[1],20,20))
+        if p.pos.x+10+p.vel.x > self.pos.x-10 and p.pos.x-10 < self.pos.x+10 and p.pos.y+10 > self.pos.y-10 and p.pos.y-10 < self.pos.y+10 and p.pos.x < self.pos.x:
             p.pos.x = self.pos.x-20
             p.vel.x = 0
             p.moveState = "still"
-        if p.pos.x-10+p.vel.x < self.pos.x+10 and p.pos.x+10 > self.pos.x-10 and p.pos.y+10 > self.pos.y-10 and p.pos.y-10 < self.pos.y+10:
+        if p.pos.x-10+p.vel.x < self.pos.x+10 and p.pos.x+10 > self.pos.x-10 and p.pos.y+10 > self.pos.y-10 and p.pos.y-10 < self.pos.y+10 and p.pos.x > self.pos.x:
             p.pos.x = self.pos.x+20
             p.vel.x = 0
             p.moveState = "still"
@@ -44,15 +46,21 @@ b = block(parametric.orderedPair(450,240))
 b2 = block(parametric.orderedPair(450,220))
 b3 = block(parametric.orderedPair(450,200))
 b4 = block(parametric.orderedPair(450,180))
+rp = False
+lp = False
+multiJump = False
 while True:
     p.fall = True
     screen.fill((255,255,255))
     p.disp()
+    if rp and lp or not (rp and lp):
+        p.moveState = "still"
+    if rp and not lp:
+        p.moveState = "right"
+    elif lp and not rp:
+        p.moveState = "left"
     for i in range(len(bL)):
         bL[i].disp(p)
-    #b.disp(p)
-    #b2.disp(p)
-    #b3.disp(p)
     b4.disp(p)
     if p.moveState == "right":
         p.vel.x += 0.25
@@ -75,6 +83,8 @@ while True:
         p.vel.y += 0.25
     p.pos.x += p.vel.x
     p.pos.y += p.vel.y
+    cam[0] += ((-p.pos.x+320)-cam[0])*0.1
+    cam[1] += ((-p.pos.y+240)-cam[1])*0.1
     pygame.display.update()
     time.sleep(1/60)
     for event in pygame.event.get():
@@ -83,13 +93,15 @@ while True:
             sys.exit()
         elif event.type == KEYDOWN:
             if event.key == K_RIGHT:
-                p.moveState = "right"
+                rp = True
             if event.key == K_LEFT:
-                p.moveState = "left"
+                lp = True
             if event.key == K_UP:
-                if not p.fall:
+                if not p.fall or multiJump:
                     p.vel.y = -5
         elif event.type == KEYUP:
-            if event.key == K_RIGHT or event.key == K_LEFT:
-                p.moveState = "still"
+            if event.key == K_RIGHT:
+                rp = False
+            if event.key == K_LEFT:
+                lp = False
         
